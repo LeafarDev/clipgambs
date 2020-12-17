fef66f3b74ba8cba355bfdfd46d641bd8bfed04a
rafael <rafael.oliveira@ignicaodigital.com.br>
2020-12-17 17:52:05 -0300

    initial clojure code

---
+++ test/clipgambs_desktop/core_test.clj
@@ -0,0 +1,7 @@
+(ns clipgambs-desktop.core-test
+  (:require [clojure.test :refer :all]
+            [clipgambs-desktop.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
